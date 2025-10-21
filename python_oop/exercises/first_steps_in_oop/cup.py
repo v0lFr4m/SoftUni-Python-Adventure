c1f0380680d4c6c8df31cbe6345ac303da795347
v0lFr4m <iphoneivan14@gmail.com>
2025-10-22 01:04:35 +0300

    First steps in OOP - exercises solutions

---
+++ python_oop/exercises/first_steps_in_oop/cup.py
@@ -0,0 +1,21 @@
+class Cup:
+    def __init__(self, size:int , quantity: int):
+        self.size = size
+        self.quantity = quantity
+
+    def fill(self, quantity):
+        if (self.quantity + quantity) <= self.size:
+            self.quantity += quantity
+            return self.quantity
+
+    def status(self):
+        free_space = self.size - self.quantity
+        return free_space
+
+
+
+cup = Cup(100, 50)
+print(cup.status())
+cup.fill(40)
+cup.fill(20)
+print(cup.status())
